802b20abc985532211fc0ac2e1f05bd78d9f4222
sdc mac <jielin.zhang447@gmail.com>
2018-03-27 19:37:07 +0800

    Initialize project. Designed posts page for index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
+import { WeUiModule } from 'ngx-weui';
+import { GalleryModule } from 'ngx-weui/gallery';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { PostsComponent } from './posts/posts.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    PostsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    WeUiModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
